aa345707606a77f95b511b93e32feb3d8edf0ba9
Kevin <kev.chu.pro@gmail.com>
2020-04-08 12:55:51 -0700

    refactor to add variables and constants

--- cs_142_java/module1/GasCalculator.java
+++ cs_142_java/module1/GasCalculator.java
@@ -1,27 +1,34 @@
 public class GasCalculator {
     
+    public static final double LITER_CONVERSION = 3.78541;
+    public static final double MILES_CONVERSION = 1.609;
+    public static double cadConversion = 0.71;
+    public static double distanceBetweenCities = 121.0;
+    public static double milesPerGallon = 36.0;
+    public static double cadGasPrice = 1.049;
+
     public static double cadToUsd(double dollarsCad) {
         // Converts from Canadian to USD
-        return dollarsCad * 0.71;
+        return dollarsCad * cadConversion;
     }
 
     public static double galToLiters(double gallons) {
         // Converts from gallons to liters
-        return gallons * 3.78541;
+        return gallons * LITER_CONVERSION;
     }
 
     public static double kmToMiles(double distanceKm) {
         // Converts from kilometers to miles
-        return distanceKm / 1.609;
+        return distanceKm / MILES_CONVERSION;
     }
     public static void main(String[] args) {
         // Compute gas used (in Liters)
-        double distanceMi = kmToMiles(121.0);
-        double gallonsOfGas = distanceMi / 36.0;
+        double distanceMi = kmToMiles(distanceBetweenCities);
+        double gallonsOfGas = distanceMi / milesPerGallon;
         double litersOfGas = galToLiters(gallonsOfGas);
 
         // Compute cost in CAD
-        double costOfGasCad = litersOfGas * 1.049;
+        double costOfGasCad = litersOfGas * cadGasPrice;
 
         // Total cost in USD
         double costOfGasUsd = cadToUsd(costOfGasCad);
