500aba9839c731093e6417f0b226ab0898d2b7c1
Kevin <kev.chu.pro@gmail.com>
2020-04-08 13:58:05 -0700

    scope example

---
+++ cs_142_java/module1/EverythingFeet.java
@@ -0,0 +1,15 @@
+public class EverythingFeet {
+	public static void main(String[] args) {
+    }
+}
+		//converts inches to feet
+		//12 inches per foot
+
+		//converts yards to feet
+		//3 feet per yard
+		
+		//converts miles to feet
+		//5280 feet per mile
+
+		//converts kilometers to feet
+		//use old code
