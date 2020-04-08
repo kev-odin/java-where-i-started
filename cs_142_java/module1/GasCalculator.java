1892d2d2df0cbd0da814fa92dcf9d269d9ab0b99
Kevin <kev.chu.pro@gmail.com>
2020-04-08 09:55:00 -0700

    prof example

--- cs_142_java/module1/GasCalculator.java
+++ cs_142_java/module1/GasCalculator.java
@@ -1,19 +1,31 @@
 public class GasCalculator {
-    public static void main(String[] args) {
-        // Price conversion
+    
+    public static double cadToUsd(double dollarsCad) {
+        // Converts from Canadian to USD
+        return dollarsCad * 0.71;
     }
-    // Find distance in miles traveled
-    public double distanceOfTravelMi() {
-        double distanceKm = 121.0;
-        double convertToMiles = 1/1.0609;
-        double distanceOfTravelMi = distanceKm * convertToMiles;
-    }
-    // Cost of gallons needed for trip
-    public gallonsForTrip; {
-        double gasMileage = 36.0;
-        return distanceOfTravelMi / gasMileage;
-    }
-    // Distance for the trip
-    public double
 
+    public static double galToLiters(double gallons) {
+        // Converts from gallons to liters
+        return gallons * 3.78541;
+    }
+
+    public static double kmToMiles(double distanceKm) {
+        // Converts from kilometers to miles
+        return distanceKm / 1.609;
+    }
+    public static void main(String[] args) {
+        // Compute gas used (in Liters)
+        double distanceMi = kmToMiles(121.0);
+        double gallonsOfGas = distanceMi / 36.0;
+        double litersOfGas = galToLiters(gallonsOfGas);
+
+        // Compute cost in CAD
+        double costOfGasCad = litersOfGas * 1.049;
+
+        // Total cost in USD
+        double costOfGasUsd = cadToUsd(costOfGasCad);
+
+        System.out.println("The total cost of gas from Vancouver BC to Whistler is: " + costOfGasUsd + " US dollars.");
+    }
 }
