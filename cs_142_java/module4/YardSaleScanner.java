74bb570200861b80914455bbfc0261b5073dbc2f
kev-odin <kev.chu.pro@gmail.com>
2020-05-21 11:30:25 -0700

    module 4!

---
+++ cs_142_java/module4/YardSaleScanner.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class YardSaleScanner {
+	
+	public static void main(String[] args) {
+		// in the main method, you should create a new scanner
+		// and call the yardSale method to test your code.
+	}
+	
+	public static double yardSale(Scanner s, double money) {
+
+		
+		return 0.0; //change this to the correct amount
+	}
+
+}
