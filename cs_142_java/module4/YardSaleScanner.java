061408340a0f036f144badc2abcc81f4f46af964
kev-odin <kev.chu.pro@gmail.com>
2020-05-22 13:58:26 -0700

    Decent working code, almost done

--- cs_142_java/module4/YardSaleScanner.java
+++ cs_142_java/module4/YardSaleScanner.java
@@ -1,19 +1,46 @@
 import java.util.Scanner;
+
 // Kevin Chung
 public class YardSaleScanner {
-	
-	// In the main method, you should create a new scanner and call the yardSale method to test your code.
+
+	// In the main method, you should create a new scanner and call the yardSale
+	// method to test your code.
 
 	public static void main(String[] args) {
-		System.out.println("Please enter the amount: ");
 		Scanner input = new Scanner(System.in);
-		double dollars = input.nextDouble();
-		yardSale(input, dollars);
-	}
-	
-	public static double yardSale(Scanner s, double money) {
-		
-		return money; //change this to the correct amount
+		double remainingBudget = yardSale(input, 50.75);
 	}
 
+	public static double yardSale(Scanner s, double money) {
+		double spendyCash = 0.0;
+		int quantity = 0;
+		int totalQuantity = 0;
+
+		// Money needs to be above $5.00 to keep the loop going
+		while (money > 5.0) {
+			// Asks the user the price of items at the yard sale.
+			System.out.print("Price? ");
+			spendyCash = s.nextDouble();
+
+			// Asks the user the quantity you want to purchase.
+			System.out.print("Quantity? ");
+			quantity = s.nextInt();
+
+			// Multiply the cost * quantity for total cost
+			double finalCost = spendyCash * quantity;
+
+			// Prevent a negative money value
+			if ((money - finalCost) >= 0) {
+				// Deal checker
+				if (spendyCash < 10.00) {
+					System.out.println("What a deal! I'll buy it.");
+					totalQuantity += quantity;
+					money -= finalCost;
+				}
+			}
+			System.out.print("Remaining money: $" + money + "\n");
+		}
+		System.out.println("Total quantity purchased: " + totalQuantity);
+		return money; // change this to the correct amount
+	}
 }
