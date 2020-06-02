50dfab7079ebacc45607ab54ef0ad57fae8512e4
kev-odin <kev.chu.pro@gmail.com>
2020-06-02 14:50:59 -0700

    set-up

--- cs_142_java/module5/ColorfulBouncingCircle.java
+++ cs_142_java/module5/ColorfulBouncingCircle.java
@@ -1,13 +1,19 @@
 import java.awt.Color;
 
 public class ColorfulBouncingCircle extends ColorfulCircle {
+    private double xVelocity;
+    private double yVelocity;
+    private double newWidth;
+    private double newHeight;
 
     public ColorfulBouncingCircle(double radius, double centerX, double centerY, Color color, double xVelocity, double yVelocity) {
         super(radius, centerX, centerY, color);
+        ColorfulBouncingCircle cbc = new ColorfulBouncingCircle(radius, centerX, centerY, color, xVelocity, yVelocity);
 
     }
 
     public static void setPlayingFieldSize(double newWidth, double newHeight) {
+
 
     }
 
@@ -17,7 +23,7 @@ public class ColorfulBouncingCircle extends ColorfulCircle {
 
     public boolean overlaps(Circle c) {
 
-        return true;
+    return true;
     }
 
 }
