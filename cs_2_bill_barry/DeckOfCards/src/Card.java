c9c2baed90f3c0e9755770b212a5dd2444f6df60
kev-odin <kev.chu.pro@gmail.com>
2020-06-08 17:00:45 -0700

    less interest

--- cs_2_bill_barry/DeckOfCards/src/Card.java
+++ cs_2_bill_barry/DeckOfCards/src/Card.java
@@ -4,8 +4,10 @@ public class Card {
     private final int suit;
     private final int rank;
     public static final String[] SUITS = { "Clubs", "Diamonds", "Hearts", "Spades" };
-    public static final String[] RANKS = { null, "Ace", "2", "3", "4", "5", "6", "7", 
-                                        "8", "9", "10", "Jack", "Queen", "King" };
+    public static final String[] RANKS = { null, "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen",
+            "King" };
+    
+    static Card[] cards = new Card[52];
 
     // Constructors
     public Card(int rank, int suit) {
@@ -18,8 +20,22 @@ public class Card {
         return s;
     }
 
+    // Populate the cards array with references to Card objects
+    public static Card[] makeDeck() {
+        int index = 0;
+        for (int suit = 0; suit <= 3; suit++) {
+            for (int rank = 1; rank <= 13; rank++) {
+                cards[index] = new Card(rank, suit);
+                index++;
+            }
+        }
+        return cards;
+    }
+
     // Read the compareTo docs, -1 means less, 1 means more, 0 means the same
     public int compareTo(Card other) {
+        if (this.rank == 1 && other.rank == 13) // Aces are high cards
+            return 1;
         if (this.suit < other.suit)
             return -1;
         if (this.suit > other.suit)
@@ -32,8 +48,9 @@ public class Card {
     }
 
     public static void printDeck(Card[] cards) {
-        for (Card card : cards) { System.out.println(card);
-        } 
+        for (Card card : cards) {
+            System.out.println(card);
+        }
     }
 
     // We want this object to be immutable
--- cs_2_bill_barry/DeckOfCards/src/Main.java
+++ cs_2_bill_barry/DeckOfCards/src/Main.java
@@ -1,16 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        
-        // Populate the cards array with references to a Card object
-        Card[] cards = new Card[52];
 
-        int index = 0;
-        for (int suit = 0; suit <= 3; suit++) {
-            for (int rank = 1; rank <= 13; rank++) { 
-                cards[index] = new Card(rank, suit); 
-                index++;
-            }
-        }
-        Card.printDeck(cards);
     }
 }
