aaa186dcd33895dd3ac0858bb61e36d3075052dc
kev-odin <kev.chu.pro@gmail.com>
2020-06-13 15:33:17 -0700

    Suite Booking 2D Array

---
+++ cs_2_bill_barry/BookingCalendar/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // Create the array to hold the status, 2D array(kinda useful)
+        Status[][] suiteStatus = new Status[12][31];
+    }
+}
