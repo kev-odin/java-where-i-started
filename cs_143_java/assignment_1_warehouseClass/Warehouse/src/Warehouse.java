065ada92d8bd24964b3a9b1d81afd53ae1af0532
kev-odin <kev.chu.pro@gmail.com>
2020-09-24 16:40:25 -0700

    full test added and warehouse object

--- cs_143_java/assignment_1_warehouseClass/Warehouse/src/Warehouse.java
+++ cs_143_java/assignment_1_warehouseClass/Warehouse/src/Warehouse.java
@@ -1,12 +1,24 @@
 public class Warehouse {
     private int size;
     private int limitPerItem;
+    private int[] storageRoom;
+
     public Warehouse(int size, int limitPerItem) {
         this.size = size;
         this.limitPerItem = limitPerItem;
+        storageRoom = new int[size];
+    }
+
+    public static void main(String[] args) {
+        Warehouse A = new Warehouse(3, 2);
+        System.out.println(A);
+        A.receive(1, 3);
     }
 
     public int receive(int itemCode, int itemCount) {
+        for (int item = 0; item <= itemCount - 1; item++) {
+            storageRoom[item] = itemCode;
+        }
         return 0;
     }
 
@@ -28,5 +40,8 @@ public class Warehouse {
 
     public void setLimitPerItem(int limitPerItem) {
         this.limitPerItem = limitPerItem;
+    }
+    public String toString() {
+        return "Warehouse size: " + size + "\nWarehouse limit: " + limitPerItem;
     }
 }
