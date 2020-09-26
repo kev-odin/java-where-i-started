0d91ac07aa4205f6a93efd0ad45e19ecd1bbd99b
kev-odin <kev.chu.pro@gmail.com>
2020-09-26 12:05:37 -0700

    practice it

--- cs_practice/projects/MovieShelf/src/ifElseMystery.java
+++ cs_practice/projects/MovieShelf/src/ifElseMystery.java
@@ -1,11 +1,18 @@
 public class ifElseMystery {
     public static void main(String[] args) {
-        mystery1(8); 	
-        mystery1(-3); 	
-        mystery1(1); 	
-        mystery1(0);
+        showTwos(7);
     }
-    
+
+    public static void showTwos(int x) {
+        System.out.print(x + " = ");
+        while (x > 0) {
+            if (x % 2 == 1) {
+                System.out.print(x + "\n");
+            }
+            x -= 2;
+        }
+    }
+
     public static void mystery1(int n) {
         System.out.print(n + " ");
         if (n > 0) {
@@ -17,5 +24,18 @@ public class ifElseMystery {
             n = n * 2;
         }
         System.out.println(n);
+    }
+
+    public static int mystery2(int x) {
+        int a = 1;
+        int c = 0;
+        while (x > 0) {
+            a = x % 2;
+            if (a == 1) {
+                c++;
+            }
+            x = x / 2;
+        }
+        return c;
     }
 }
