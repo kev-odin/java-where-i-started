3d26c3b71300f683f131fccdc632ae7cbde9139f
kev <kev.chu.pro@gmail.com>
2021-01-04 10:36:04 -0800

    clean-up old cs143_fall

--- cs_2_bill_barry/BankProject/src/BankAccountTest.java
+++ cs_2_bill_barry/BankProject/src/BankAccountTest.java
@@ -1,9 +1,7 @@
-import static org.junit.Assert.*;
 
 public class BankAccountTest {
     public void testConstructorAndGets() {
         BankAccount testAccount = new BankAccount("123", "Moog Loaf", 1078.89);
-        assertEquals("123", testAccount.getOwnerName());
     }
     
 }
