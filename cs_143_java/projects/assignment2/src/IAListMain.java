1f5018ca2da6ceffe64cab9f3c5d384ed8f46de4
kev <kev.chu.pro@gmail.com>
2021-01-26 15:55:20 -0800

    18/100 kinda works?

--- cs_143_java/projects/assignment2/src/IAListMain.java
+++ cs_143_java/projects/assignment2/src/IAListMain.java
@@ -1,11 +1,16 @@
 public class IAListMain {
     public static void main(String[] args) {
         IAList a = new IAList();
-
-        for (int i = 0; i <= 4; i++) {
-            a.add(i);
-        }
-
-        a.addBefore(2);
+        a.add(1);
+        a.add(2);
+        a.add(3);
+        a.add(4);
+        a.addBefore(5);
+        a.addBefore(6);
+        a.addBefore(7);
+        a.addBefore(8);
+        a.add(9);
+        a.add(10);
+        System.out.println(a.size());
     }
 }
