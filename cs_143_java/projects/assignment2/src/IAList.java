49711824a8012cef575c9cc2d310b8b54693c221
kev <kev.chu.pro@gmail.com>
2021-01-27 13:32:55 -0800

    100/100 - no idea why?

--- cs_143_java/projects/assignment2/src/IAList.java
+++ cs_143_java/projects/assignment2/src/IAList.java
@@ -11,56 +11,58 @@ public class IAList {
 		header = 0; // YOU MAY NEED TO INITIALIZE YOUR NEW DATA FIELD(S).
 	}
 
-	public int get(int i) { // THE NEW DATA FIELD(S) MAY CHANGE THE WAY GET WORKS.
-		if (i < 0 || i >= length) { // Retrieve an added element, O(1)
-			throw new IndexOutOfBoundsException(i + "");
-		}
-		return a[i]; // Retrieve the element at position i
-	}
-
-	public int size() { // Number of added elements, O(1)
-		// the data does not start are the beginning of the array
-		// THE NEW DATA FIELD(S) MAY CHANGE THE WAY SIZE WORKS.
-		return length; // The number of added elements to array
-	}
-
-	public void set(int i, int x) { // Modify an existing element, O(1)
-		if (i < 0 || i >= a.length) { // THE NEW DATA FIELD(S) MAY CHANGE THE WAY SET WORKS.
-			throw new IndexOutOfBoundsException(i + "");
-		}
-		a[i] = x; // Change the existing element at position i to x
-	}
-
 	public void add(int x) { // Add an element to the end, O(n) for n
-		if (length >= a.length) { // THE NEW DATA FIELD(S) MAY CHANGE THE WAY ADD WORKS.
+		if (length + header >= a.length) { // THE NEW DATA FIELD(S) MAY CHANGE THE WAY ADD WORKS.
 			int[] b = new int[a.length * 2]; // Create new array of double the length
 			for (int i = header; i < a.length; i++) { // Copy the elements of a to the corresponding indexes of b
 				b[i] = a[i];
 			}
 			a = b; // Reassign a reference to b
 		}
-		a[length] = x; // Place x at the end of the IAList
+		a[length + header] = x; // Place x at the end of the continuous data
 		length++; // Increase length by 1
 	}
 
 	public void addBefore(int x) {
-		int maxSize = a.length;
-		if (length >= a.length || header == 0 && length == maxSize) {		// No space availible for addBefore OR at the start
-			int[] b = new int[a.length * 2];	//TODO: Shift the index + 1
-			header = b.length - a.length;
-			for (int i = b.length - a.length; i < b.length; i++) {				// Copy my items in the array from the tail to head
+		if (length >= a.length || header == 0) { // No space availible for addBefore OR at the start
+			int[] b = new int[a.length * 2]; // Double length of array a
+			header = b.length - 1; // Header is starting at the end of array
+			for (int i = header; i >= a.length; i--) { // Copy my items in the array from the tail to head
 				b[i] = a[i - a.length];
 				header--;
 			}
-			length++;
 			b[header] = x;
-			a = b;
-		} else if (length < maxSize && header == 0) {
-			for (int i = a.length - 1; i > 0; i--) {
-				a[i] = a[i - 1];
-			}
 			length++;
+			a = b; // Copy array
+		} else if (length < a.length && header > 0) { // Room in the array to place element and data does not start at 0
+			header--;
 			a[header] = x;
+			length++;
 		}
 	}
+
+	public int get(int i) { // THE NEW DATA FIELD(S) MAY CHANGE THE WAY GET WORKS. (Retrieve an added element, O(1))
+		if (i < 0|| i >= length) {
+			throw new IndexOutOfBoundsException(i + "");
+		}
+		if (header > 0 && length + header <= a.length) {
+			i = i + header;
+		}
+		return a[i]; // Retrieve the element at position i
+	}
+
+	public int size() { // THE NEW DATA FIELD(S) MAY CHANGE THE WAY SIZE WORKS.(Number of added elements, O(1))
+		return length; // The number of added elements to array
+	}
+
+	public void set(int i, int x) { // THE NEW DATA FIELD(S) MAY CHANGE THE WAY SET WORKS. (Modify an existing element, O(1))
+		if (i < 0 || i >= a.length) { 
+			throw new IndexOutOfBoundsException(i + "");
+		}
+		if (header > 0) {
+			i = i + header;
+		}
+		a[i] = x; // Change the existing element at position i to x
+	}
+
 }
--- cs_143_java/projects/assignment2/src/IAListMain.java
+++ cs_143_java/projects/assignment2/src/IAListMain.java
@@ -1,13 +1,25 @@
 public class IAListMain {
     public static void main(String[] args) {
         IAList a = new IAList();
+        // a.add(1);
+        // a.add(2);
+        // a.add(3);
+        // a.add(4);
+        // a.addBefore(5);
+        // a.addBefore(6);
+        // a.addBefore(7);
+        // a.addBefore(8);
+        // a.add(9);
+        // a.addBefore(10);
+        // a.addBefore(11);
+        // a.add(12);
+        // a.addBefore(13);
+        // a.add(14);
+
         a.add(1);
         a.add(2);
         a.add(3);
-        a.add(4);
+        a.addBefore(4);
         a.addBefore(5);
-        a.addBefore(6);
-        System.out.println(a.get(0));
-        System.out.println(a.size());
     }
 }
