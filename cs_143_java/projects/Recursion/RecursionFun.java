668d75e881c49afe7e7f9c57a687015f35544122
Kevin Chung <kev.chu.pro@gmail.com>
2021-02-05 13:48:29 -0800

    solution for exercise

--- cs_143_java/projects/Recursion/RecursionFun.java
+++ cs_143_java/projects/Recursion/RecursionFun.java
@@ -1,16 +1,30 @@
 public class RecursionFun {
     public static void main(String[] args) {
-        System.out.println(mystery(10));
-        System.out.println(mystery(15));
-        System.out.println(mystery(25));
-        System.out.println(mystery(-14));
+        System.out.println(countOnes(1111));
+        System.out.println(countOnes(1));
+        System.out.println(countOnes(-1));
+        System.out.println(countOnes(0));
+        System.out.println(countOnes(-121));
+        System.out.println(countOnes(2131415));
+    }
 
-
+    public static int countOnes(int n) {
+        if (n < 0) {
+            return countOnes(-n);
+        } else if (n % 10 == 1) {
+            return 1 + countOnes(n / 10);
+        } else if (n > 0) {
+            return countOnes(n / 10);
+        } else {
+            return 0;
+        }
     }
 
     public static int mystery(int a) {
-        if (a < 0) return mystery(-a);
-        if (a == 0) return 0;
-        return a % 10 + mystery(a/10);
-      }
+        if (a < 0)
+            return mystery(-a);
+        if (a == 0)
+            return 0;
+        return a % 10 + mystery(a / 10);
+    }
 }
