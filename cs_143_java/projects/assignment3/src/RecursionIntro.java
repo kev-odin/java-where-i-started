a79e6683bfc5095c0c8eda6a0a7c1dd12fa46289
Kevin Chung <kev.chu.pro@gmail.com>
2021-02-06 15:59:32 -0800

    33 / 100 Two problems done!

--- cs_143_java/projects/assignment3/src/RecursionIntro.java
+++ cs_143_java/projects/assignment3/src/RecursionIntro.java
@@ -4,30 +4,47 @@ public class RecursionIntro {
 
     public static long eduodd(long n) {
         if (n < 0) {
-            return -eduodd(-n);
-        } else if (n < 10 && n % 2 == 0) { // even up
-            return (n + 1) % 10;
-        } else if (n < 10 && n % 2 == 1) { // odd down
-            return (n - 1) % 10;
-        } else {
-            return 10 * eduodd(n / 10) + (n + 1) % 10;
+            return -eduodd(-n); // if n is negative
+        } else if (n > 10) {
+            return 10 * eduodd(n / 10);
+        } else if (n < 10) {
+            if (n % 2 == 0) { 
+                return (n + 1) % 10; // even numbers
+            } else {
+                return (n - 1) % 10; // odd numbers
+            }
         }
+        return n;
     }
 
-    public static int fibby(int n) {
-        return 0;
+    public static int fibby(int n) { // DONE
+        // base case
+        if (n == 0) {
+            return 1;
+        } else {
+            return fibby(n / 3) + fibby((2 * n) / 3);
+        }
     }
 
     public static void printSparseTable(int start, int end) {
 
     }
 
-    private static void printSparseTable(int start, int end, int fibby) {
+    private static void printSparseTable(int start, int end, int prevFib) {
 
     }
 
-    public static int lp2lt(int n) {
-        return 0;
+    public static int lp2lt(int n) { // DONE
+        // base case
+        if (n == 2) {
+            return 1;
+        // even case
+        } else if (n > 2 && n % 2 == 0) {
+            return 2 * lp2lt(n / 2);
+        // odd case
+        } else {
+            return 2 * lp2lt((n + 1) / 2);
+        }
     }
 
     public static int champion(boolean[] a) {
--- cs_143_java/projects/assignment3/src/RecursionMain.java
+++ cs_143_java/projects/assignment3/src/RecursionMain.java
@@ -1,13 +1,16 @@
 public class RecursionMain {
     public static void main(String[] args) {
-        System.out.println(mystery(-6));
-        System.out.println(mystery(63));
-        System.out.println(mystery(-763));
-        System.out.println(mystery(8888));
-        System.out.println(eduodd(27));         // = 36
-        System.out.println(eduodd(987654321));  // = 896745230
-        System.out.println(eduodd(-11));        // = 0
-
+        //System.out.println(mystery(-6));
+        //System.out.println(mystery(63));
+        //System.out.println(mystery(-763));
+        //System.out.println(mystery(8888));
+        // System.out.println(eduodd(1)); // 0
+        // System.out.println(eduodd(2)); // 3
+        // System.out.println(eduodd(27));         // = 36
+        // System.out.println(eduodd(987654321));  // = 896745230
+        // System.out.println(eduodd(-11));        // = 0
+        // System.out.println(lp2lt(20));
+        System.out.println(fibby(100));
     }
 
     public static int mystery(int n) { // eduodd part 1; what to do when n is even or odd
@@ -20,13 +23,40 @@ public class RecursionMain {
         }
     }
 
-    public static long eduodd(long n) {
+    public static long eduodd(long n) { // WIP
         if (n < 0) {
             return -eduodd(-n); // if n is negative
-        } else if (n % 2 == 0 && n > 0) {
-            return 10 * eduodd(n / 10) + (n + 1) % 10;
+        } else if (n > 10) {
+            return 10 * eduodd(n / 10);
+        } else if (n < 10) {
+            if (n % 2 == 0) { 
+                return (n + 1) % 10; // even numbers
+            } else {
+                return (n - 1) % 10; // odd numbers
+            }
+        }
+        return n;
+    }
+
+    public static int fibby(int n) { // DONE
+        // base case
+        if (n == 0) {
+            return 1;
         } else {
-            return 10 * eduodd(n / 10) + (n - 1) % 10;
+            return fibby(n / 3) + fibby((2 * n) / 3);
+        }
+    }
+
+    public static int lp2lt(int n) { // DONE
+        // base case
+        if (n == 2) {
+            return 1;
+        // even case
+        } else if (n > 2 && n % 2 == 0) {
+            return 2 * lp2lt(n / 2);
+        // odd case
+        } else {
+            return 2 * lp2lt((n + 1) / 2);
         }
     }
 }
