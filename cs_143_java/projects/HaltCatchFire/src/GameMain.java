ca944e324e360face77b7c6a47779b1c55a75a2d
Kevin Chung <kev.chu.pro@gmail.com>
2021-02-26 18:31:27 -0800

    playing around with GameMain

--- cs_143_java/projects/HaltCatchFire/src/GameMain.java
+++ cs_143_java/projects/HaltCatchFire/src/GameMain.java
@@ -1,13 +1,34 @@
 import java.io.File;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class GameMain {
     private static File textFile;
     public static void main(String[] args) {
         textFile = new File("trivia.csv"); // May need to change the path to where you have the "trivia.csv"; check your own computer.
         QuestionList library = new QuestionList(textFile);
+        Scanner readThis = new Scanner(System.in);
+        boolean playGame = true;
 
-        System.out.println(library.triviaList.get(13));
-        
-        
+        triviaWelcome();
+
+        while(playGame) {
+
+            String s = readThis.next();
+            if (s.contains("y")) {
+                System.out.println("YO");
+            } else if (s.contains("n")) {
+                playGame = false;
+            } else {
+                throw new InputMismatchException("You entered the wrong letter and now must die!");
+            }
+        }
+    }
+
+    public static void triviaWelcome() {
+        String welcome = "You are playing the latest and greatest trivia game of this generation!";
+        String instruction = "To play this GOAT, please use the keyboard to make selections and follow the prompts.";
+        String ready = "Are you ready to play? (Y/n)";
+        System.out.println("\n" + welcome + "\n" + instruction + "\n" + ready + "\n");
     }
 }
