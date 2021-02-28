7c0ec68ea07ad6285be6e9841e0d3fa28f11ed91
kev <kev.chu.pro@gmail.com>
2021-02-27 17:13:43 -0800

    exercises from quize

--- cs_143_java/projects/Collections/src/SetPlayground.java
+++ cs_143_java/projects/Collections/src/SetPlayground.java
@@ -14,11 +14,19 @@ public class SetPlayground {
         System.out.println(s); // the set is the same, because Possum was already in the set
         s.add("Squirrel");
         // notice that that the elements are printed in alphabetical order
-        // This is a feature of TreeSets where the elements are organized in alphabetical order
+        // This is a feature of TreeSets where the elements are organized in
+        // alphabetical order
         System.out.println(s);
 
         // we can ask a set is something a member of it?, which will give us a boolean
-        System.out.println(s.contains("Squirrel")); // YES!
-        System.out.println(s.contains("Mouse")); // NO!
+        //System.out.println(s.contains("Squirrel")); // YES!
+        //System.out.println(s.contains("Mouse")); // NO!
+
+        Set<String> a = new TreeSet<String>();
+        Set<String> b = new TreeSet<String>();
+        a.add("apple"); // One "apple" added to a
+        b.add("apple");
+        b.add("apple"); // Two "apple"s added to b
+        System.out.println(b.equals(a)); // Do a and b represent the same set?
     }
 }
