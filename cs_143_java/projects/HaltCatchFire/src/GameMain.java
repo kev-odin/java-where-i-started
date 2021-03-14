1661ee217d08530a320d4e530eca4cb7cb769f58
Kevin Chung <kev.chu.pro@gmail.com>
2021-03-13 22:03:10 -0800

    working tester program

--- cs_143_java/projects/HaltCatchFire/src/GameMain.java
+++ cs_143_java/projects/HaltCatchFire/src/GameMain.java
@@ -17,9 +17,9 @@ public class GameMain {
     public static void main(String[] args) {
         // May need to change the path to the "trivia.csv"; check your own computer.
         textFile = new File("trivia.csv");
-        // Instantiate player bank account: Starting: $0 || Round 1: $1000 || Round 2:
-        // $1100;
-        BankAccount bank = new BankAccount(0, 1000, 1100);
+        // Instantiate player bank account: Starting: $0 || Round 1: $900 || Round 2:
+        // $1400;
+        BankAccount bank = new BankAccount(0, 900, 1400);
         QuestionList game = new QuestionList(textFile);
         PromptReader prompter = new PromptReader();
         GameBoardConstructor gc = new GameBoardConstructor(2, game);
