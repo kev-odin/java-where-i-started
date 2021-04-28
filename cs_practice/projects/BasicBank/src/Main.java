12d5f6f9321f34fa55dd996b46652baf9abd1f4d
Kevin Chung <kev.chu.pro@gmail.com>
2021-04-28 10:45:34 -0700

    cleaning up shop

--- cs_practice/projects/BasicBank/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        BankAccount firstAccount = new BankAccount("Bourbon", "1", 22.50);
-
-        System.out.println(firstAccount);
-    }
-    
-}
